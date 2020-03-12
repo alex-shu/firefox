febbe841b40fb4000b2d2f2b71bfb5151546d2a3
Лебедев Андрей Николаевич <ALebedev@galex.ru>
2020-03-12 11:51:18 +0700

    add Alt Linux support

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,7 +13,7 @@ class firefox (
   Boolean    $shumway_disabled                            = true,
   Boolean    $plugins_notifyMissingFlash                  = false,
   Boolean    $datareporting_healthreport_service_enabled  = false,
-  Boolean    $datareporting_policy_dataSubmissionEnabled  = false, 
+  Boolean    $datareporting_policy_dataSubmissionEnabled  = false,
   Boolean    $toolkit_telemetry_enabled                   = false,
   Boolean    $toolkit_telemetry_rejected                  = true,
   Integer    $toolkit_telemetry_prompted                  = 2,
@@ -23,13 +23,15 @@ class firefox (
              $proxy_settings                              = undef,
 ){
 case $operatingsystem {
-      centos, redhat: { $pathfirefox = "/usr/lib64/firefox/"
-                        $pathpref = "${pathfirefox}defaults/preferences" }
-      debian, ubuntu: { $pathfirefox = "/usr/lib/firefox"
+      centos, redhat: { $pathfirefox = '/usr/lib64/firefox'
+                        $pathpref = "${pathfirefox}/defaults/preferences" }
+      debian, ubuntu: { $pathfirefox = '/usr/lib/firefox'
                         $pathpref = "${pathfirefox}/defaults/pref" }
-      linuxmint:      { $pathfirefox = "/usr/lib/firefox"
+      linuxmint:      { $pathfirefox = '/usr/lib/firefox'
                         $pathpref = "${pathfirefox}/defaults/pref" }
-      default: { fail("Unrecognized operating system for webserver") }
+      alt:            { $pathfirefox = '/usr/lib64/firefox'
+                        $pathpref = "${pathfirefox}/defaults/pref" }
+      default: { fail('Unrecognized operating system for webserver') }
 }
 
 package {
@@ -38,24 +40,23 @@ package {
 }
 
 file { 'mozilla.cfg':
-        path => "$pathfirefox/mozilla.cfg",
         ensure  => file,
+        path    => "${pathfirefox}/mozilla.cfg",
         content => template('firefox/mozilla.cfg.erb'),
-        mode => '644',
-        owner => 'root',
-        group => 'root',
+        mode    => '0644',
+        owner   => 'root',
+        group   => 'root',
         #require => Class['firefox::install'],
 }
 
 file { 'all-settings.js':
-         path => "$pathpref/all-settings.js",
          ensure  => file,
-         source => 'puppet:///modules/firefox/all-settings.js',
-         mode => '644',
-         owner => 'root',
-         group => 'root',
-         require => File["$pathfirefox/mozilla.cfg"]
+         path    => "${pathpref}/all-settings.js",
+         source  => 'puppet:///modules/firefox/all-settings.js',
+         mode    => '0644',
+         owner   => 'root',
+         group   => 'root',
+         require => File["${pathfirefox}/mozilla.cfg"]
 }
 
 }
-
